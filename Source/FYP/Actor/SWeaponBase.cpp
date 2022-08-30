a2f8ecfdece399a79aea005272f2ce3420a64c5d
Ben <benxian456@gmail.com>
2022-08-30 19:49:48 +0800

    first commit

---
+++ Source/FYP/Actor/SWeaponBase.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SWeaponBase.h"
+
+// Sets default values
+ASWeaponBase::ASWeaponBase()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASWeaponBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASWeaponBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/FYP/Character/SCharacter.cpp
@@ -0,0 +1,64 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SCharacter.h"
+
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+
+// Sets default values
+ASCharacter::ASCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>("Camera Boom");
+	CameraBoom->SetupAttachment(RootComponent);
+	Camera = CreateDefaultSubobject<UCameraComponent>("Camera");
+	Camera->SetupAttachment(CameraBoom);
+	bUseControllerRotationYaw=false;
+	CameraBoom->bUsePawnControlRotation=true;
+	GetCharacterMovement()->bOrientRotationToMovement=true;
+}
+
+// Called when the game starts or when spawned
+void ASCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAxis("Move Forward / Backward",this,&ASCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("Move Right / Left",this,&ASCharacter::MoveRight);
+	PlayerInputComponent->BindAxis("Turn Right / Left Mouse",this,&APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("Look Up / Down Mouse",this,&APawn::AddControllerPitchInput);
+
+	PlayerInputComponent->BindAction("Jump",EInputEvent::IE_Pressed,this,&ACharacter::Jump);
+}
+
+void ASCharacter::MoveForward(float val)
+{
+	FRotator ControlRot=GetControlRotation();
+	ControlRot.Pitch=0;
+	ControlRot.Roll=0;
+	AddMovementInput(ControlRot.Vector(),val);
+}
+void ASCharacter::MoveRight(float val)
+{
+	FRotator ControlRot=GetControlRotation();
+	ControlRot.Pitch=0;
+	ControlRot.Roll=0;
+
+	FVector ControlRightVec=UKismetMathLibrary::GetRightVector(ControlRot);
+	AddMovementInput(ControlRightVec,val);
+}
